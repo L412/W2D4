85c807e921e6aac0aa6f0b1421dbb0c1640ca4de
L412 <skylarprill@gmail.com>
2018-11-01 14:31:10 -0700

    Write largest contiguous sum methods with O(n^2) and O(n)

--- execution_time_differences/execution_time_differences.rb
+++ execution_time_differences/execution_time_differences.rb
@@ -15,8 +15,6 @@ def my_min_1(arr)
   min
 end
 
-
-
 #This one is for  O(n)
 def my_min_2(arr)
   min = arr[0]
@@ -29,4 +27,54 @@ def my_min_2(arr)
   min
 end
 
-p my_min_2(LIST)
+
+LIST2 = [2, 3, -6, 7, -6, 7]
+
+def sub_sum_1(arr)
+  subsets = []
+  i = 0
+
+  while i < arr.length
+    j = i
+    while j < arr.length
+      subsets << arr[i..j]
+      j += 1
+    end
+    i += 1
+  end
+
+  subsets.map! do |arr|
+    arr.reduce(:+)
+  end
+
+  subsets.max
+end
+
+
+def sub_sum_2(arr)
+  ongoing_sum = nil
+  current_best = arr[0]
+
+  arr.each do |el| #Big-O(n) because we iterate through array once
+
+    if el > current_best
+      current_best = el
+    end
+
+    if !(ongoing_sum)
+      ongoing_sum = el
+    elsif (el + ongoing_sum) > ongoing_sum
+      ongoing_sum += el
+      current_best = ongoing_sum if ongoing_sum > current_best
+    elsif ongoing_sum + el > 0
+      ongoing_sum += el
+    else
+      ongoing_sum = nil
+    end
+  end
+
+  current_best
+end
+
+
+p sub_sum_2([-5, -1, -3])
