f04b6b11d08ecf3d944028e4995cbe4d1dda1b5b
L412 <skylarprill@gmail.com>
2018-11-01 16:53:34 -0700

    Create hash_map_two_sum solution O(n)

--- two_sum/two_sum.rb
+++ two_sum/two_sum.rb
@@ -16,10 +16,34 @@ def brute_force_two_sum(arr, target_sum)
   false
 end
 
+#Starting with a pre sorted array.
+#Would use merge sort for O(nlogn)
 def sorting_two_sum(arr, target_sum)
 
-  
-
+  sorted_arr = arr #O(nlogn)
+  sorted_arr.each_with_index do |el,idx|
+    temp_arr = sorted_arr[0...idx] + sorted_arr[idx+1..-1]
+    return true if temp_arr.bsearch {|x| x + el == target_sum}
+  end
+  false
 end
 
-p brute_force_two_sum([0, 1, 5, 7], 6)
+
+def hash_map_two_sum(arr, target_sum)
+
+  hash = Hash.new
+
+  arr.each do |el|
+    hash[el] = target_sum - el
+  end
+
+  hash.each do |k, val|
+    if hash.has_key?(val) && k != val
+      return true
+    elsif k == val
+      return false
+    end
+  end
+end
+# p sorting_two_sum([0, 1, 5, 7], 10)
+p hash_map_two_sum([0, 1, 5, 7], 6)
